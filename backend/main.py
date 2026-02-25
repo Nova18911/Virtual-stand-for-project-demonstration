49d93f054dcf0d7421d36c7664507c1851a2c179
xfenixxf <x.fenix.x.f@gmail.com>
2026-02-25 20:52:40 +0300

    3
    mainpage.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,52 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template
+=======
+from flask import Flask,redirect, render_template, url_for
+from auth import auth_bp
+from core.registration import register_bp
+from core.mainpage import mainpage_bp
+
+>>>>>>> Stashed changes
 
 # Указываем Flask, где искать папки со страницами и стилями
 app = Flask(__name__,
             template_folder='../frontend/templates',
             static_folder='../frontend/static')
 
+<<<<<<< Updated upstream
 @app.route('/register')
 def register_page():
     return render_template('registration.html')
 
+=======
+app.register_blueprint(auth_bp)
+app.register_blueprint(register_bp)
+app.register_blueprint(mainpage_bp)
+
+@app.route('/')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/reg')
+def register():
+    return redirect(url_for('register_page'))
+
+@app.route('/registertion')
+def register_page():
+    return render_template('registration.html')
+
+@app.route('/regist')
+def register_redirect():
+    return redirect(url_for('login_page'))
+
+@app.route('/main')
+def main_page():
+    return render_template('mainpage.html')
+
+@app.route('/mainpage')
+def main_redirect():
+    return redirect(url_for('main_page'))
+
+>>>>>>> Stashed changes
 if __name__ == '__main__':
     app.run(debug=True)
