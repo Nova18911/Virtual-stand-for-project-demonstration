1d0240caefbe13e1297edb4cd43e7a3bdefc96d3
Nova18911 <hypern0va70343007@gmail.com>
2026-04-09 19:31:16 +0300

    fix

--- backend/core/admin_main.py
+++ backend/core/admin_main.py
@@ -11,7 +11,7 @@ def get_db():
         port=5432,
         database='course_management',
         user='postgres',
-        password='12345'
+        password='12345678'
     )
 def rows_to_dicts(cursor, rows):
     if rows is None:
--- backend/core/connect.py
+++ backend/core/connect.py
@@ -3,7 +3,7 @@ import pg8000
 DB_HOST = 'localhost'
 DB_PORT = 5432
 DB_NAME = 'course_management'
-DB_USER = 'admin'
+DB_USER = 'postgres'
 DB_PASS = '12345678'
 
 def get_db_connection():
